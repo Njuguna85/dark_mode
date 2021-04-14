fcd309dda2fa538c6bca850bdaaf6125cef3396a
Njuguna85 <dennix.njuguna85@gmail.com>
2021-04-14 16:28:41 +0300

    vue dark/light mode

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app" :class="mode == 'dark' ? 'dark' : 'light'">
+    <the-header :mode="mode" @toggle="toggle"></the-header>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TheHeader from "./components/TheHeader.vue";
 export default {
-  name: 'App',
+  data() {
+    return {
+      mode: "light",
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    TheHeader,
+  },
+  methods: {
+    toggle() {
+      if (this.mode === "dark") {
+        this.mode = "light";
+      } else {
+        this.mode = "dark";
+      }
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+    "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+}
+
+.app {
+  width: 100vw;
+  min-height: 100vh;
+  background-color: #f3f3f3;
+  color: rgba(11, 11, 39, 0.87);
+  transition: all 0.3s ease-in-out;
+}
+
+.dark {
+  background-color: rgba(11, 11, 39, 0.87);
+  color: #f3f3f3;
 }
 </style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,59 @@
+<template>
+  <header>
+    <h1>{{ mode }} Mode</h1>
+    <!-- toggle -->
+    <toggle :mode="mode" @change="$emit('toggle')"></toggle>
+    <nav>
+      <div class="nav-link">Home</div>
+      <div class="nav-link">About</div>
+      <div class="nav-link">Portfolio</div>
+      <div class="nav-link">Contact</div>
+    </nav>
+  </header>
+</template>
+
+<script>
+import Toggle from "./Toggle.vue";
+export default {
+  props: ["mode"],
+  components: {
+    Toggle,
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 25px;
+  height: 60px;
+  background: #c3c3c3;
+  transition: all 0.3s ease-in-out;
+}
+.dark header {
+  background: rgba(11, 11, 39, 0.87);
+}
+header h1 {
+  font-size: 32px;
+  text-transform: uppercase;
+  font-weight: 900;
+}
+
+header nav {
+  display: flex;
+  height: 60px;
+}
+header nav .nav-link {
+  display: flex;
+  align-items: center;
+  padding: 0 15px;
+}
+header nav .nav-link:hover {
+  background: #f3f3f3;
+}
+.dark header nav .nav-link:hover {
+  background: rgba(22, 22, 97, 0.87);
+}
+</style>
